6877c5b851ce0989e7764136d7ee59aacc3ca313
oussemabhouri00 <mohamedoussema.bhouri@ensi-uma.tn>
2021-06-04 11:21:10 +0100

    added kubedb

--- main.go
+++ main.go
@@ -3,24 +3,33 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
+
+type Deployment struct {
+	DeploymentNamespace string `json:"DeploymentNamespace"`
+	Replicas            int    `json:"Replicas"`
+}
 
 type Pod struct {
 	PodName        string `json:"PodName"`
 	ContainerName  string `json:"ContainerName"`
 	ContainerImage string `json:"ContainerImage"`
-	PodNamespace   string `json:"Namespace"`
 }
 
 type Namespace struct {
@@ -34,15 +43,85 @@ func returnAllPods(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Pods)
 }
 
+//------------------------------------------------------------------------------------------------------------------------
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to the HomePage!")
-	fmt.Println("Endpoint Hit: homePage")
+//------------------------------------------------------------------------------------------------------------------------
+
+func createNewdeployment(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var deploy Deployment
+	json.Unmarshal(reqBody, &deploy)
+	/*rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+	config, err := kubeconfig.ClientConfig()
+	if err != nil {
+		panic(err)
+	}*/
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+	client, err := dynamic.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	//clientset := kubernetes.NewForConfigOrDie(config)
+	deploymentRes := schema.GroupVersionResource{Group: "kubedb.com", Version: "v1alpha1", Resource: "postgreses"}
+	deployment := &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "kubedb.com/v1alpha1",
+			"kind":       "Postgres",
+			"metadata": map[string]interface{}{
+				"name":      "postgres-deployment",
+				"namespace": deploy.DeploymentNamespace,
+			},
+			"spec": map[string]interface{}{
+				"version":       "11.1",
+				"replicas":      deploy.Replicas,
+				"standbyMode":   "Hot",
+				"streamingMode": "asynchronous",
+				"leaderElection": map[string]interface{}{
+					"leaseDurationSeconds": 5,
+					"renewDeadlineSeconds": 3,
+					"retryPeriodSeconds":   2,
+				},
+				"storageType": "Durable",
+				"storage": map[string]interface{}{
+					"storageClassName": "nas",
+					"accessModes":      []string{"ReadWriteMany"},
+					"resources": map[string]interface{}{
+						"requests": map[string]interface{}{
+							"storage": "1Gi",
+						},
+					},
+				},
+				"terminationPolicy": "WipeOut",
+			},
+		},
+	}
+	// Create Deployment
+	fmt.Println("Creating deployment...")
+	result, err := client.Resource(deploymentRes).Namespace(deploy.DeploymentNamespace).Create(context.TODO(), deployment, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Created deployment %q.\n", result.GetName())
 }
+
+//------------------------------------------------------------------------------------------------------------------------
 
 func createNewPod(w http.ResponseWriter, r *http.Request) {
 	// get the body of our POST request
@@ -50,58 +129,62 @@ func createNewPod(w http.ResponseWriter, r *http.Request) {
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	var pod Pod
 	json.Unmarshal(reqBody, &pod)
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+	config, err := kubeconfig.ClientConfig()
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
-	// creates the clientsettt
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
+	clientset := kubernetes.NewForConfigOrDie(config)
 	newPod := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: pod.PodName,
 		},
 		Spec: corev1.PodSpec{
 			Containers: []corev1.Container{
-				{Name: pod.ContainerName, Image: pod.ContainerImage, Command: []string{"sleep", "100000"}},
+				{Name: "nginx", Image: "nginx:latest", Command: []string{"sleep", "100000"}},
 			},
 		},
 	}
-
-	createdPod, err := clientset.CoreV1().Pods(pod.PodNamespace).Create(context.Background(), newPod, metav1.CreateOptions{})
-
+	createdPod, err := clientset.CoreV1().Pods("pg-namespace").Create(context.Background(), newPod, metav1.CreateOptions{})
 	fmt.Println(createdPod)
+	Pods = []Pod{
+		Pod{PodName: "Hello", ContainerName: "Pod ContainerName", ContainerImage: "Pod ContainerImage"},
+	}
 	fmt.Fprintf(w, "%+v", string(pod.PodName))
 }
+
+//------------------------------------------------------------------------------------------------------------------------
 
 func createNewNamespace(w http.ResponseWriter, r *http.Request) {
 
 	reqBody, _ := ioutil.ReadAll(r.Body)
-	var namespace Pod
+	var namespace Namespace
 	json.Unmarshal(reqBody, &namespace)
-	config, err := rest.InClusterConfig()
+	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+	kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+	config, err := kubeconfig.ClientConfig()
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+	clientset := kubernetes.NewForConfigOrDie(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
 	newNamespace := &corev1.Namespace{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "My-namespacee",
+			Name: namespace.NsName,
 		},
 	}
 
 	createdNs, err := clientset.CoreV1().Namespaces().Create(context.Background(), newNamespace, metav1.CreateOptions{})
 	fmt.Println(createdNs)
+	fmt.Fprintf(w, "%+v", string(namespace.NsName))
 
 }
+
+//------------------------------------------------------------------------------------------------------------------------
 
 func handleRequests() {
 	// creates a new instance of a mux router
@@ -114,12 +197,20 @@ func handleRequests() {
 	myRouter.HandleFunc("/pods", returnAllPods).Methods("GET")
 	//myRouter.HandleFunc("/pod", createNewPod).Methods("POST")
 	myRouter.HandleFunc("/namespace", createNewNamespace).Methods(("POST"))
+	myRouter.HandleFunc("/postgres", createNewdeployment).Methods(("POST"))
+
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
-
 func main() {
 
-	/*
+	/*	rules := clientcmd.NewDefaultClientConfigLoadingRules()
+		kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(rules, &clientcmd.ConfigOverrides{})
+		config, err := kubeconfig.ClientConfig()
+		if err != nil {
+			panic(err)
+		}
+		clientset := kubernetes.NewForConfigOrDie(config)
+
 		nodeList, err := clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
 		if err != nil {
 			panic(err)
@@ -127,13 +218,8 @@ func main() {
 		for _, n := range nodeList.Items {
 			fmt.Println(n.Name)
 		}
-
 		if err != nil {
 			panic(err)
-		}*/
-	Pods = []Pod{
-		Pod{PodName: "Hello", ContainerName: "Pod ContainerName", ContainerImage: "Pod ContainerImage"},
-	}
-
-	handleRequests()
+		}
+	*/handleRequests()
 }
